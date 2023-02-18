56e12bd6e86ec0dbd57bdcec5792a6ea41da8a95
oayouub <91758300+oayouub@users.noreply.github.com>
2023-02-18 11:38:45 +0100

    Revert "push gang"
    This reverts commit 3ac68960021fd6602044766ca96aa54a70deef43.

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,26 +1,15 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const connectDB = async () => {
-  try {
-    mongoose.set("strictQuery", false);
-    mongoose.connect(process.env.MONGO_URI, () => console.log("mongo conecté"));
-  } catch (error) {
-    console.log(error);
-    process.exit();
-  }
+    try {
+        mongoose.set('strictQuery', false);
+        mongoose.connect(process.env.MONGO_URI, () => console.log("mongo conecté"))
+    } catch(error ) {
+        console.log(error);
+        process.exit();
+    }
 };
 
-const connectDBUser = async () => {
-  try {
-    mongoose.set("strictQuery", false);
-    mongoose.connect(process.env.MONGO_URI_USER, () =>
-      console.log("mongo conecté")
-    );
-  } catch (error) {
-    console.log(error);
-    process.exit();
-  }
-};
 
 module.exports = connectDB;
-module.exports = connectDBUser;
+
--- backend/server.js
+++ backend/server.js
@@ -1,51 +1,28 @@
 const express = require("express");
 const connectDB = require("./config/db");
-const cors = require("cors");
+const cors = require('cors');
 const dotenv = require("dotenv").config();
 const port = 5000;
-const mongoose = require("mongoose");
-const connectDBUser = require("./config/db");
-const bcrypt = require("bcryptjs");
 
 connectDB();
-connectDBUser();
 
 const app = express();
-require("./models/user.model");
 
 app.use(
-  cors({
-    origin: "http://localhost:3000",
-    credentials: true,
-    optionsSuccessStatus: 200,
-  })
+    cors({
+        origin: "http://localhost:3000",
+        credentials: true,
+        optionsSuccessStatus: 200,
+    })
 );
-
-const User = mongoose.model("UserInfo");
-app.post("/register", async (req, res) => {
-  try {
-    const oldUser = await User.findOne({ email });
-
-    if (oldUser) {
-      return res.json({ error: "User Exists" });
-    }
-    await User.create({
-      fname: req.body.fname,
-      lname: req.body.lname,
-      email: req.body.email,
-      password: req.body.password,
-      userType: req.body.userType,
-    });
-    res.send({ status: "ok" });
-  } catch (error) {
-    res.send({ status: "error" });
-  }
-});
 
 // Middleware qui permet de traiter les données de la Request
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use("/", require("./routes/produit.routes"));
+app.use("/", require('./routes/produit.routes'));
+
+
+
 
 // Lancer le serveur
 app.listen(port, () => console.log("Le serveur a démarré au port  " + port));
